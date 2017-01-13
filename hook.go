03f35c5ba78d6853bf8890c3e90e480c6928631e
Marcin Wyszynski <marcin.pixie@gmail.com>
2017-01-13 21:50:58 +0100

    Actual code

---
+++ hook.go
@@ -0,0 +1,54 @@
+package logrus3164
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+)
+
+const format = "Jan 2 15:04:05"
+
+type hookImpl struct {
+	io.Writer
+	hostname string
+	tag      string
+}
+
+func NewHook(writer io.Writer, tag string) (logrus.Hook, error) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return err
+	}
+	return &hookImpl{writer, hostname, tag}, nil
+}
+
+func (*hookImpl) Levels() []logrus.Level {
+	return []logrus.Level{
+		logrus.PanicLevel,
+		logrus.FatalLevel,
+		logrus.ErrorLevel,
+		logrus.WarnLevel,
+		logrus.InfoLevel,
+		logrus.DebugLevel,
+	}
+}
+
+func (hook *hookImpl) Fire(*logrus.Entry) error {
+	date := time.Now().Format(format)
+	msg, err := entry.String()
+	if err != nil {
+		return err
+	}
+	payload := fmt.Sprintf(
+		"<22>%s %s %s: %s",
+		date,
+		hook.hostname,
+		hook.tag,
+		msg,
+	)
+	_, err := hook.Write([]byte(payload))
+	return err
+}
